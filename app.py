2624c5e52825b7d910c4d33938821295f95c43de
Prathamesh J. Pawar <prathmeshpawar212002@gmail.com>
2024-11-29 23:28:25 +0530

    Model Create

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,redirect
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    
+    return render_template("index.html")
+
+
+
+
+if  __name__ == "__main__":
+    print("Hello this is medicine recommendation system")
+    # app.run(debug=True)
+    
