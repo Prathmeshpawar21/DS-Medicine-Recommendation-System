30cc945c1fb5be730f5196ba617b6a1e48456e4d
Prathamesh J. Pawar <prathmeshpawar212002@gmail.com>
2024-12-01 14:30:05 +0530

    UI BUild

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from research import *
 app = Flask(__name__)
 
 
-@app.route('/', methods=['POST','GET'])
+@app.route('/index.html', methods=['POST','GET'])
 def index():
     userInputSymptomList=""
     PredictedPersonDisease = ""
@@ -24,14 +24,29 @@ def index():
         
         
         
-        
-        
+    
         
     return render_template("index.html",userInputSymptomList=userInputSymptomList,PredictedPersonDisease=PredictedPersonDisease,desc=desc, diet=diet, med=med, prec=prec,  work=work)
 
 
 
+# @app.route("/index.html")
+# def index():
+#     return render_template("index.html")
+    
+@app.route("/about.html")
+def about():
+    return render_template("about.html")
 
+@app.route("/departments.html")
+def departments():
+    return render_template("departments.html")
+
+@app.route("/contact.html")
+def contact():
+    return render_template("contact.html")
+
+    
 if __name__ == "__main__":
     app.run(debug=True)
     
